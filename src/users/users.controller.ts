e0a6fd89394e69ed4f60f07e7ec1005e5478823e
karimDevWM <67164906+karimDevWM@users.noreply.github.com>
2023-05-01 11:47:36 +0200

    - disable eslint in the file - import Body and Post decorator - import CreateUserDto - update the main controller route name - make a Post request route - add comment for explaining the Body decorator

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+/* eslint-disable prettier/prettier */
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateUserDto } from './dtos/create-user.dto';
 
-@Controller('users')
-export class UsersController {}
+@Controller('auth')
+export class UsersController {
+
+    @Post('/signup')
+    // Body decorator is used to tell to nest extract the body from the incoming request
+        // so here the extrcted body is gonna be of type CreateUserDto
+    createUser(@Body() body: CreateUserDto) {
+        console.log(body);
+    }
+}
