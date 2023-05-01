9215f11247bccb616e516390ca8cae07f9cae91f
karimDevWM <67164906+karimDevWM@users.noreply.github.com>
2023-05-01 10:50:23 +0200

    add comments to explain the meaning of each decorator, and his action into the database table

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,12 +1,14 @@
 /* eslint-disable prettier/prettier */
 import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
-
+// that tells to typeOrm if you look at this class make a realtive table in db
 @Entity()
 export class User {
+    // this gonna create a column type number in table db that 
+        // his identifier is auto incremeted and assigned to a recorded data
     @PrimaryGeneratedColumn()
     id: number;
-
+    // this create a column type string into db table
     @Column()
     email: string;
 
