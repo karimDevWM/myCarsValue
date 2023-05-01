f05ab6c0fc77eab45376a2e38ff4f260b14cfa50
karimDevWM <67164906+karimDevWM@users.noreply.github.com>
2023-05-01 10:48:34 +0200

    - disabling linter for the file - import ValidationPipe - adding a method  useGloablPipes from the class ValidationPipe with the option whitelist to true

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
+/* eslint-disable prettier/prettier */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      // this tell to nestjs, on extracting the body incoming request
+        // you return only the attribte that they refer to the dto here
+      whitelist: true,
+    })
+  );
   await app.listen(3000);
 }
 bootstrap();
