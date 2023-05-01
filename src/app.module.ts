45a2062140bf93738a8a720020034861254c6883
karimDevWM <67164906+karimDevWM@users.noreply.github.com>
2023-05-01 10:45:24 +0200

    add explanation on synchronize typeOrm option

--- src/app.module.ts
+++ src/app.module.ts
@@ -17,6 +17,7 @@ import { Report } from './reports/report.entity';
         type: 'sqlite',
         database: 'db.sqlite',
         entities: [User, Report],
+        // used for updating automatocally Database table if there is a structure changes, unstade of migration
         synchronize: true,
       }
     )
